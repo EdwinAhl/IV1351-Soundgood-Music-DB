f562a5574afa3abb6f211dc3eafd392b89ac2842
Edwin <edwin.ahlstrand@bahnhof.se>
2022-11-23 15:20:52 +0100

    added contact person

--- SoundgoodMusicDB.sql
+++ SoundgoodMusicDB.sql
@@ -1,12 +1,12 @@
 CREATE TABLE person (
  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- ssn UNIQUE VARCHAR(12) NOT NULL,
+ ssn VARCHAR(12) NOT NULL,
  name VARCHAR(100) NOT NULL,
  street VARCHAR(100) NOT NULL,
  zip VARCHAR(100) NOT NULL,
  city VARCHAR(100) NOT NULL,
- phone VARCHAR(11) NOT NULL,
- mail VARCHAR(100) NOT NULL
+ mail VARCHAR(100) NOT NULL,
+ student_id INT GENERATED ALWAYS AS IDENTITY
 );
 
 ALTER TABLE person ADD CONSTRAINT PK_person PRIMARY KEY (id);
@@ -14,7 +14,7 @@ ALTER TABLE person ADD CONSTRAINT PK_person PRIMARY KEY (id);
 
 CREATE TABLE phone (
  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- phone_no VARCHAR(100)
+ phone_no VARCHAR(100) NOT NULL
 );
 
 ALTER TABLE phone ADD CONSTRAINT PK_phone PRIMARY KEY (id);
@@ -22,18 +22,17 @@ ALTER TABLE phone ADD CONSTRAINT PK_phone PRIMARY KEY (id);
 
 CREATE TABLE price (
  difficulty VARCHAR(100) NOT NULL,
- lesson_type VARCHAR(100) NOT NULL,
- basePrice INT NOT NULL
+ base_price INT NOT NULL
 );
 
-ALTER TABLE price ADD CONSTRAINT PK_price PRIMARY KEY (difficulty,lesson_type);
+ALTER TABLE price ADD CONSTRAINT PK_price PRIMARY KEY (difficulty);
 
 
 CREATE TABLE student (
  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
  person_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
  skill VARCHAR(100),
- instrumentToLearn VARCHAR(100)
+ instrument_to_learn VARCHAR(100)
 );
 
 ALTER TABLE student ADD CONSTRAINT PK_student PRIMARY KEY (id);
@@ -59,21 +58,20 @@ ALTER TABLE instructor ADD CONSTRAINT PK_instructor PRIMARY KEY (id);
 
 
 CREATE TABLE lease (
- id INT NOT NULL,
+ id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
  student_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
  period INTERVAL(100) NOT NULL
 );
 
-ALTER TABLE lease ADD CONSTRAINT PK_lease PRIMARY KEY (id,student_id);
+ALTER TABLE lease ADD CONSTRAINT PK_lease PRIMARY KEY (id);
 
 
 CREATE TABLE lesson (
  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
  booking_id INT GENERATED ALWAYS AS IDENTITY,
- student_id INT GENERATED ALWAYS AS IDENTITY,
  instructor_id INT GENERATED ALWAYS AS IDENTITY,
+ student_id INT GENERATED ALWAYS AS IDENTITY,
  difficulty VARCHAR(100) NOT NULL,
- lesson_type VARCHAR(100) NOT NULL,
  instrument VARCHAR(100) NOT NULL
 );
 
@@ -90,8 +88,7 @@ ALTER TABLE person_phone ADD CONSTRAINT PK_person_phone PRIMARY KEY (person_id,p
 
 CREATE TABLE rentable_instrument (
  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- lease_id INT,
- student_id INT GENERATED ALWAYS AS IDENTITY,
+ lease_id INT GENERATED ALWAYS AS IDENTITY,
  price INT NOT NULL,
  quality VARCHAR(100) NOT NULL,
  brand VARCHAR(100)
@@ -110,8 +107,8 @@ ALTER TABLE sibling_student ADD CONSTRAINT PK_sibling_student PRIMARY KEY (id,st
 
 CREATE TABLE group_lesson (
  lesson_id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
- minStudents VARCHAR(100) NOT NULL,
- maxStudents VARCHAR(100) NOT NULL,
+ min_students INT NOT NULL,
+ max_students INT NOT NULL,
  period INTERVAL(100) NOT NULL
 );
 
@@ -134,6 +131,9 @@ CREATE TABLE ensamble (
 ALTER TABLE ensamble ADD CONSTRAINT PK_ensamble PRIMARY KEY (lesson_id);
 
 
+ALTER TABLE person ADD CONSTRAINT FK_person_0 FOREIGN KEY (student_id) REFERENCES student (id);
+
+
 ALTER TABLE student ADD CONSTRAINT FK_student_0 FOREIGN KEY (person_id) REFERENCES person (id);
 
 
@@ -148,14 +148,14 @@ ALTER TABLE lease ADD CONSTRAINT FK_lease_0 FOREIGN KEY (student_id) REFERENCES 
 
 ALTER TABLE lesson ADD CONSTRAINT FK_lesson_0 FOREIGN KEY (booking_id,student_id) REFERENCES booking (id,student_id);
 ALTER TABLE lesson ADD CONSTRAINT FK_lesson_1 FOREIGN KEY (instructor_id) REFERENCES instructor (id);
-ALTER TABLE lesson ADD CONSTRAINT FK_lesson_2 FOREIGN KEY (difficulty,lesson_type) REFERENCES price (difficulty,lesson_type);
+ALTER TABLE lesson ADD CONSTRAINT FK_lesson_2 FOREIGN KEY (difficulty) REFERENCES price (difficulty);
 
 
 ALTER TABLE person_phone ADD CONSTRAINT FK_person_phone_0 FOREIGN KEY (person_id) REFERENCES person (id);
 ALTER TABLE person_phone ADD CONSTRAINT FK_person_phone_1 FOREIGN KEY (phone_id) REFERENCES phone (id);
 
 
-ALTER TABLE rentable_instrument ADD CONSTRAINT FK_rentable_instrument_0 FOREIGN KEY (lease_id,student_id) REFERENCES lease (id,student_id);
+ALTER TABLE rentable_instrument ADD CONSTRAINT FK_rentable_instrument_0 FOREIGN KEY (lease_id) REFERENCES lease (id);
 
 
 ALTER TABLE sibling_student ADD CONSTRAINT FK_sibling_student_0 FOREIGN KEY (id) REFERENCES student (id);

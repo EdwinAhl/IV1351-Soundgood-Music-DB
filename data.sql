55af5fcc61ceb9ee27aa2a46f2c4505e03f97f7f
Edwin <edwin.ahlstrand@bahnhof.se>
2022-11-24 18:07:40 +0100

    more phone more person more good

--- data.sql
+++ data.sql
@@ -4,16 +4,32 @@ VALUES
 ('198401011234', 'Baltzar Lagerros', 'Vägen2', '987 65', 'Borås', 'baltzarl@kth.se'),
 ('201401011234','Medge Francis','Ap #381-534 Orci Rd.','88-455','Port Blair','fringilla@google.edu'),
 ('199401011234','Palmer Gay','Ap #810-4547 Bibendum Road','87882','Mastung','et.libero@outlook.couk');
+('199601011234','Emmanuel Higgins','464-5072 Aliquet Rd.','90246','Stellenbosch','nec.metus.facilisis@protonmail.com'),
+('202001011234','Wyatt Barker','Ap #380-7152 Sit St.','61418','Aisén','dolor.fusce@outlook.com'),
+('202001011234','Iris Lott','Ap #996-5511 Facilisis Av.','13516','Bhiwani','vehicula@icloud.org'),
+('200401011234','Kirsten Peterson','Ap #938-2688 Nunc St.','650117','Devonport','risus.odio.auctor@yahoo.com'),
+('200301011234','Odessa Austin','P.O. Box 460, 6293 Donec St.','563248','Mâcon','enim@aol.ca');
 
 INSERT INTO phone(phone_no)
 VALUES
 ('1-873-883-0217'),
 ('(211) 613-7391'),
 ('1-355-932-3693'),
-('(861) 634-8528');
+('(861) 634-8528'),
+('(918) 780-1939'),
+('(971) 664-7537'),
+('1-651-497-1123'),
+('1-743-678-2647'),
 
 INSERT INTO person_phone(person_id, phone_id)
 VALUES
+(1, 1),
+(1, 2),
+(2, 3),
+(2, 4),
+(3, 5),
+(4, 6);
+
 
 INSERT INTO instructor(person_id, skill, instrument, available)
 VALUES

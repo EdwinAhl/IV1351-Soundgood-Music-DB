e2173f14f6f882fa41097adeda783a85f1f8f548
Edwin <edwin.ahlstrand@bahnhof.se>
2022-11-29 14:07:05 +0100

    created queries.sql

---
+++ queries.sql
@@ -0,0 +1,10 @@
+--Show the number of lessons given per month during a specified year.
+
+
+--Show how many students there are with no sibling, with one sibling, with two siblings, etc.
+
+
+--List all instructors who has given more than a specific number of lessons during the current month. Sum all lessons, independent of type, and sort the result by the number of given lessons.
+
+
+--List all ensembles held during the next week, sorted by music genre and weekday. For each ensemble tell whether it's full booked, has 1-2 seats left or has more seats left.

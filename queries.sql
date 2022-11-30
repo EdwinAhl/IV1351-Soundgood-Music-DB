512ba48c1fa9d041cfe5794c77bac441f3cac6ac
Edwin <edwin.ahlstrand@bahnhof.se>
2022-11-30 14:07:01 +0100

    yes

--- queries.sql
+++ queries.sql
@@ -1,11 +1,11 @@
 --A1: Show the number of lessons given per month during a specified year -------------------------------------
 
-SELECT COUNT(*) FROM
+SELECT COUNT(*) AS no_lessons FROM
 	(SELECT start_time FROM group_lesson 
 		UNION ALL 
 	SELECT start_time FROM individual_lesson) as times
 	WHERE 
-		EXTRACT(YEAR FROM start_time::timestamp)=2020 and 
+		EXTRACT(YEAR FROM start_time::timestamp)=2020 AND 
 		EXTRACT(MONTH FROM start_time::timestamp)=03
 ;
 
